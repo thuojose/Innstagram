5a3cdfe5fa3bd9927a20aa0814cceaf487a78e09
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-17 18:46:29 +0300

    created likeform/comment form in view.py

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import login,views, forms
 from django.contrib.auth.decorators import login_required
+from .forms import LikesForm, CommentsForm
 
 # Create your views here.
 #landing page
 def landing(request):
     form = forms.AuthenticationForm
     return render(request, 'landing_page.html', locals())
+
+#home page
+@login_required(login_url='/accounts/login')
+def home(request):
+    likesForm = LikesForm
+    commentForm = CommentsForm
+    
+    
+    return render(request,'instagram_pages/home.html', locals())
