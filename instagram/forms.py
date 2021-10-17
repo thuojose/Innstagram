5cddd310d5b06de3ef66e9a774427ba20ac12adc
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-17 20:00:03 +0300

    added classes for profile update/pic upload in form.py

--- instagram/forms.py
+++ instagram/forms.py
@@ -10,3 +10,17 @@ class LikesForm(forms.Form):
 class CommentsForm(forms.ModelForm):
     class Meta:
         model = Comment
+        exclude = ['image', 'user']
+        
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields=['prof_pic','bio']
+        
+        
+class UploadPicForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        # imagefile = Image.image=('Select a file')
+        fields=('image','name','caption',)
+        # exclude = ['profile','post_date','user']
