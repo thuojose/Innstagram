0c8735c14a3b4664b0a9b7d14ab1b684fc3d4cfd
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-17 18:42:08 +0300

    added url patter for home

---
+++ instagram/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+urlpatterns = [
+    path('',views.landing,name='instagramLandingPage'),
+    path('home/',views.home,name='instagramHome'),
+    
+     ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
