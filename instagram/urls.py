2a1c24b88c7ff451dde83e1ce45349c8fe84c5ab
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-17 19:21:23 +0300

    added url pattern for search

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 urlpatterns = [
     path('',views.landing,name='instagramLandingPage'),
     path('home/',views.home,name='instagramHome'),
+    path('search/', views.search_results,name='search_results'),
     path('like/<int:image_id>',views.likes,name='likes'),
     path('comment/<int:image_id>',views.comments,name='comments'),
     
