73c8fe4408a3d2c216b87340ed7fb0a6407e687e
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-17 18:05:59 +0300

    additional changes in different files

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+#landing page
+def landing(request):
+    form = forms.AuthenticationForm
+    return render(request, 'landing_page.html', locals())
